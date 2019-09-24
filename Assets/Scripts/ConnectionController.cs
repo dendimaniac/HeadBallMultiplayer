2664e23ee2b2f2d0f16639f963f276940f2640e9
Dendi Maniac <daq30899@gmail.com>
2019-09-24 13:55:21 +0300

    Added basic connections, spawning player

---
+++ Assets/Scripts/ConnectionController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using Bolt.Matchmaking;
+using System;
+using UdpKit;
+
+public class ConnectionController : Bolt.GlobalEventListener
+{
+    public void StartServer()
+    {
+        if (BoltNetwork.IsRunning)
+        {
+            BoltLauncher.Shutdown();
+        }
+        BoltLauncher.StartServer();
+    }
+
+    public void StartClient()
+    {
+        if (BoltNetwork.IsRunning)
+        {
+            BoltLauncher.Shutdown();
+        }
+        BoltLauncher.StartClient();
+    }
+
+    public override void BoltStartDone()
+    {
+        if (BoltNetwork.IsServer)
+        {
+            string matchName = Guid.NewGuid().ToString();
+
+            BoltMatchmaking.CreateSession(matchName, null, "Game");
+        }
+    }
+
+    public override void SessionListUpdated(Map<Guid, UdpSession> sessionList)
+    {
+        Debug.LogFormat("Session list updated: {0} total sessions", sessionList.Count);
+
+        foreach (var session in sessionList)
+        {
+            UdpSession photonSession = session.Value as UdpSession;
+
+            if (photonSession.Source == UdpSessionSource.Photon)
+            {
+                BoltNetwork.Connect(photonSession);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/NetworkCallbacks.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[BoltGlobalBehaviour]
+public class NetworkCallbacks : Bolt.GlobalEventListener
+{
+    public override void SceneLoadLocalDone(string scene)
+    {
+        // randomize a position
+        var spawnPosition = new Vector2(Random.Range(-2f, 2f), -1f);
+
+        // instantiate cube
+        BoltNetwork.Instantiate(BoltPrefabs.Player, spawnPosition, Quaternion.identity);
+    }
+}
